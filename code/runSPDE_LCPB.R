7f639f91abec5965af5d2fc68fd3cfc5a5b786a0
John Paige <paigejo@gmail.com>
2020-10-20 11:32:26 -0700

    fixed rgdal load for the cluster

--- code/runSPDE_LCPB.R
+++ code/runSPDE_LCPB.R
@@ -1,3 +1,4 @@
+library("rgdal", lib.loc="/sw/ebpkgs/software/rgdal/1.4-8-foss-2019b-R-3.6.2")
 source("setup.R")
 index = as.numeric(commandArgs(trailingOnly = TRUE))
 load("savedOutput/simDataSets/spde_lcpbCommandArgs.RData")
